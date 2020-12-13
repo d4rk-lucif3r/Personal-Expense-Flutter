b99ae2ad30431246ae56d789641769becff509d6
Arsh Anwar <lucifer78908@gmail.com>
2020-12-14 00:29:46 +0530

    Removed Unnecessary Imports
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
-import '../providers/transactionProvider.dart';
 
 class NewTransactions extends StatefulWidget {
   final Function addTx;
